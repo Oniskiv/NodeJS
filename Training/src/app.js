1352fcefbabc6f236234dee5316b375fb60521df
Oniskiv Vladislav <onisckif.vladislaw@gmail.com>
2018-05-06 14:20:31 +0300

    Task 2 is done.

--- Training/src/app.js
+++ Training/src/app.js
@@ -1,7 +1,12 @@
 import config from "./../config/configuration";
 import user from "./models/user";
 import product from "./models/product";
+import dirwatcher from "./modules/dirwatcher";
+import importer from "./modules/importer";
 
 console.log(config.name);
 var userObj = new user();
 var productObj = new product();
+
+var dirwatcherObj = new dirwatcher(config.path, config.delay);
+var importerObj = new importer(dirwatcherObj, config.path);
